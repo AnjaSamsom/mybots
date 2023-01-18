4206c8d4661ec0f518705fc4b001aa08da4a0bb5
AnjaSamsom <anjavt14@gmail.com>
2023-01-18 09:40:32 -0500

    made window stay open with sleep function

--- simulate.py
+++ simulate.py
@@ -1,6 +1,12 @@
 import pybullet as p
+import time
 
 # This creates an object, physicsClient, which handles the physics, 
 # and draws the results to a Graphical User Interface (GUI).
 physicsClient = p.connect(p.GUI)
+
+for i in range(1001):
+    time.sleep(1/60)
+    p.stepSimulation()
+    print(i)
 p.disconnect()
