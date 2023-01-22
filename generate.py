e71e2a8640ee2e18dda8cc4025e75f98e1609b51
AnjaSamsom <anjavt14@gmail.com>
2023-01-22 16:37:20 -0500

    made gravity, changed height, length, and width to variables

--- generate.py
+++ generate.py
@@ -8,7 +8,11 @@ import pyrosim.pyrosim as pyrosim
 # contain a box (links can be spheres, cylinders, or boxes).
 pyrosim.Start_SDF("box.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length = 1
+height = 1
+width = 1
+
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, height, width])
 
 pyrosim.End()
 
--- simulate.py
+++ simulate.py
@@ -1,10 +1,14 @@
 import pybullet as p
 import time
+import pybullet_data
 
 # This creates an object, physicsClient, which handles the physics, 
 # and draws the results to a Graphical User Interface (GUI).
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 for i in range(1001):
     time.sleep(1/60)
