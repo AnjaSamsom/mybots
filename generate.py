acdd85554e0b0e45ce9a9c91ade5afc61a13ae2d
AnjaSamsom <anjavt14@gmail.com>
2023-01-22 22:35:03 -0500

    made stack of 10 cubes

--- generate.py
+++ generate.py
@@ -6,14 +6,28 @@ import pyrosim.pyrosim as pyrosim
 # world you're about to create should be stored. 
 # This world will currently be called box, because it will only 
 # contain a box (links can be spheres, cylinders, or boxes).
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
 length = 1
 height = 1
 width = 1
 
+x = 0
+y = 0.5
+z = 0
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, width, height])
+for i in range(10):
+    pyrosim.Send_Cube(name="Box", pos=[x,z,y+i] , size=[length, width, height])
+
+
+
+# x2 = 1
+# y2 = 1.5
+# z2 = 0
+
+# pyrosim.Send_Cube(name="Box", pos=[x,z,y] , size=[length, width, height])
+# pyrosim.Send_Cube(name="Box 2", pos=[x2,z2,y2] , size=[length, width, height])
+
 
 pyrosim.End()
 
--- simulate.py
+++ simulate.py
@@ -9,7 +9,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 for i in range(2000):
     time.sleep(1/60)
     p.stepSimulation()
