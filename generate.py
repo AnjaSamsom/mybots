297ea9079496c014fd06fb8082ca11721ead3c56
AnjaSamsom <anjavt14@gmail.com>
2023-01-25 18:29:24 -0500

    working on joints

--- generate.py
+++ generate.py
@@ -5,32 +5,22 @@ import pyrosim.pyrosim as pyrosim
 # world you're about to create should be stored. 
 # This world will currently be called box, because it will only 
 # contain a box (links can be spheres, cylinders, or boxes).
-pyrosim.Start_SDF("boxes.sdf")
 
-length = 1
-height = 1
-width = 1
 
-for w in range(5):
-    for l in range (5):
-        x = 0+l
-        z = 0.5
-        y = 0+w
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[-4,4,0.5] , size=[1,1,1])
+    pyrosim.End()
 
-        length = 1
-        height = 1
-        width = 1
 
-        for i in range(10):
-                
-            length = length * 0.9
-            height = height  * 0.9
-            width = width  * 0.9
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5,0,1])
+    pyrosim.Send_Cube(name="Leg", pos=[1.0,0,1.5] , size=[1,1,1])
 
-            if i == 0:
-                length = height = width = 1
+    pyrosim.End()   
 
-            pyrosim.Send_Cube(name="Box", pos=[x,y,z+i] , size=[length, width, height])
 
-pyrosim.End()
-
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -9,7 +9,9 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+robotId = p.loadURDF("body.urdf")
+
+p.loadSDF("world.sdf")
 for i in range(2000):
     time.sleep(1/60)
     p.stepSimulation()
