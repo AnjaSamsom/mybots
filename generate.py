f1620946d677530f7d2fbb44e63231a7492aff9d
AnjaSamsom <anjavt14@gmail.com>
2023-01-25 17:08:19 -0500

    changed dimension names (I mixed them up), finished big structure

--- generate.py
+++ generate.py
@@ -1,7 +1,6 @@
 import pyrosim.pyrosim as pyrosim
 
 
-
 # to tell pyrosim the name of the file where information about the 
 # world you're about to create should be stored. 
 # This world will currently be called box, because it will only 
@@ -12,29 +11,18 @@ length = 1
 height = 1
 width = 1
 
-x = 0
-y = 0.5
-z = 0
-
-for a in range(5):
-    x = 0
-    y = 0.5
-    z = 0 + a
-
-    length = 1
-    height = 1
-    width = 1
-    for w in range (5):
-        x = 0+w
-        y = 0.5
-        z = 0
+for w in range(5):
+    for l in range (5):
+        x = 0+l
+        z = 0.5
+        y = 0+w
 
         length = 1
         height = 1
         width = 1
 
         for i in range(10):
-            
+                
             length = length * 0.9
             height = height  * 0.9
             width = width  * 0.9
@@ -42,17 +30,7 @@ for a in range(5):
             if i == 0:
                 length = height = width = 1
 
-            pyrosim.Send_Cube(name="Box", pos=[x,z,y+i] , size=[length, width, height])
-
-
-
-# x2 = 1
-# y2 = 1.5
-# z2 = 0
-
-# pyrosim.Send_Cube(name="Box", pos=[x,z,y] , size=[length, width, height])
-# pyrosim.Send_Cube(name="Box 2", pos=[x2,z2,y2] , size=[length, width, height])
-
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z+i] , size=[length, width, height])
 
 pyrosim.End()
 
