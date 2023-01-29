6e353762bccc6734684fb1c0cd61339092eca786
AnjaSamsom <anjavt14@gmail.com>
2023-01-29 16:02:08 -0500

    sensor vector prints

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet as p
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import numpy
 
 
 # This creates an object, physicsClient, which handles the physics, 
@@ -16,9 +17,12 @@ robotId = p.loadURDF("robot.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
-for i in range(2000):
+frontLegSensorValues = numpy.zeros(10000)
+
+for i in range(10000):
     time.sleep(1/60)
     p.stepSimulation()
-    frontLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    print(frontLegTouch)
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 p.disconnect()
+
+print(frontLegSensorValues)
