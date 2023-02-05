61d61f024ac1d2a500a0cb12afd0c03ddd56b469
AnjaSamsom <anjavt14@gmail.com>
2023-02-05 15:42:33 -0500

    fixed pyrosim and made motor

--- simulate.py
+++ simulate.py
@@ -29,6 +29,9 @@ for i in range(runs):
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_BackLeg", 
+controlMode = p.POSITION_CONTROL, targetPosition = 0.0, maxForce = 500)
+
 p.disconnect()
 
 
