009691d97381b59144e3095e22583f1812f947d2
AnjaSamsom <anjavt14@gmail.com>
2023-03-22 13:54:16 -0400

    step 22

--- solution.py
+++ solution.py
@@ -54,8 +54,8 @@ class SOLUTION:
         pyrosim.Send_Cube(name="Torso", pos=[0,0,1], size=[1,1,1])
         pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0,0.5,1])
         pyrosim.Send_Cube(name="FrontLeg", pos=[0,0.5,0], size=[0.2,1,0.2])
-        pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [-0.5,0,1])
-        pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5], size=[1,1,1])
+        pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,-0.5,1])
+        pyrosim.Send_Cube(name="BackLeg", pos=[0,-0.5,0], size=[0.2,1,0.2])
 
 
         pyrosim.End()   
